379c170441bbdbe4966bec14e93431b61da003e6
Vasilii <shores.freezer0u@icloud.com>
2023-05-22 16:19:17 +1000

    Initial Commit

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by VASILY IKONNIKOV on 22.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
