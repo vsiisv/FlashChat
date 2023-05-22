ca24c1f4bdbbd56204a9425f2a04383b6d7ded97
Vasilii <shores.freezer0u@icloud.com>
2023-05-22 17:04:12 +1000

    Add images and colors

---
+++ FlashChat/Extensions/UIColor+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extension.swift
+//  FlashChat
+//
+//  Created by VASILY IKONNIKOV on 22.05.2023.
+//
+
+import UIKit
+
+extension UIColor {
+	static let brandBlue = UIColor(named: "BrandBlue")
+	static let brandLightBlue = UIColor(named: "BrandLightBlue")
+	static let brandLightPurple = UIColor(named: "BrandLightPurple")
+	static let brandPurple = UIColor(named: "BrandPurple")
+}
---
+++ FlashChat/Extensions/UIImage+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIImage+Extension.swift
+//  FlashChat
+//
+//  Created by VASILY IKONNIKOV on 22.05.2023.
+//
+
+import UIKit
+
+extension UIImage {
+	static let yourAvatar = UIImage(named: "YouAvatar")
+	static let textfield = UIImage(named: "textField")
+	static let meAvatar = UIImage(named: "MeAvatar")
+}
